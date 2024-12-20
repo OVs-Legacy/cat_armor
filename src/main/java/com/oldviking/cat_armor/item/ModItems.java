0358472ea0ccbe472a13b2d1558a04f924a2c55b
OldViking013 <21294791+OldViking013@users.noreply.github.com>
2024-12-21 00:15:39 +0100

    Update ModItems.java
    Might not work and needs to be revisited

--- src/main/java/com/oldviking/cat_armor/item/ModItems.java
+++ src/main/java/com/oldviking/cat_armor/item/ModItems.java
@@ -3,9 +3,8 @@ package com.oldviking.cat_armor.item;
 import com.chocohead.mm.api.ClassTinkerers;
 import com.oldviking.cat_armor.CatArmor;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.client.color.item.ItemColors;
-import net.minecraft.component.type.DyedColorComponent;
 import net.minecraft.item.*;
+import net.minecraft.item.equipment.ArmorMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -13,7 +12,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item CAT_ARMOR = registerItem("cat_armor",
-            new AnimalArmorItem(ArmorMaterials.ARMADILLO, ClassTinkerers.getEnum(AnimalArmorItem.Type.class, "FELINE"), true, new Item.Settings().maxDamage(ArmorItem.Type.BODY.getMaxDamage(4))));
+            new AnimalArmorItem(ArmorMaterials.ARMADILLO_SCUTE, ClassTinkerers.getEnum(AnimalArmorItem.Type.class, "FELINE"), new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(CatArmor.MOD_ID, name), item);
