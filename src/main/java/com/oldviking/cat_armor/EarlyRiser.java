b8cd04a0e421cb1b2b283aded336f981a122f134
Old <21294791+OldViking013@users.noreply.github.com>
2025-02-10 22:23:04 +0100

    Fixed Enum Mixin

--- src/main/java/com/oldviking/cat_armor/EarlyRiser.java
+++ src/main/java/com/oldviking/cat_armor/EarlyRiser.java
@@ -3,10 +3,8 @@ package com.oldviking.cat_armor;
 import com.chocohead.mm.api.ClassTinkerers;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
+import net.minecraft.entity.EntityType;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Identifier;
-
-import java.util.function.Function;
 
 public class EarlyRiser implements Runnable {
     @Override
@@ -15,9 +13,10 @@ public class EarlyRiser implements Runnable {
 
         String mappedClassName = remapper.mapClassName("intermediary", "net.minecraft.class_4059$class_9076");
         String soundEvents = 'L' + remapper.mapClassName("intermediary", "net.minecraft.class_3414")+ ';';
-        Function<Identifier, Identifier> function = (id) -> Identifier.of("cat_armor", "textures/entity/cat/cat_armor");
-        ClassTinkerers.enumBuilder(mappedClassName, Function.class, soundEvents)
-                .addEnum("FELINE", () -> new Object[] { function, SoundEvents.ITEM_WOLF_ARMOR_BREAK })
+        String entityType = 'L' + remapper.mapClassName("intermediary", "net.minecraft.class_1299")+ ';';
+
+        ClassTinkerers.enumBuilder(mappedClassName, soundEvents, "["+entityType)
+                .addEnum("FELINE", () -> new Object[] { SoundEvents.ITEM_WOLF_ARMOR_BREAK, new EntityType[] { EntityType.CAT } })
                 .build();
     }
 }
