4af7b6866be062801b42b6eb44b40ee3941dc342
Old <21294791+OldViking013@users.noreply.github.com>
2025-02-10 22:24:04 +0100

    Removed unused imports

--- src/main/java/com/oldviking/cat_armor/datagen/ModRecipeGenerator.java
+++ src/main/java/com/oldviking/cat_armor/datagen/ModRecipeGenerator.java
@@ -5,13 +5,9 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
-import net.minecraft.data.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
